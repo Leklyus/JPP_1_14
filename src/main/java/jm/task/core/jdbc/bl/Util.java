f3adebcf70b0c05a33e7c88f03dc15f74b41bf7e
Leklyus <leklyus@yahoo.com>
2024-08-25 17:51:06 +0400

    The task is ready for verification.

--- src/main/java/jm/task/core/jdbc/bl/Util.java
+++ src/main/java/jm/task/core/jdbc/bl/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.bl;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.util.Properties;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,6 +22,31 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
+    private static final SessionFactory concreteSessionFactory;
+
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/db_pp_1_1_4");
+            prop.setProperty("hibernate.connection.username", "root");
+            prop.setProperty("hibernate.connection.password", "root");
+            prop.setProperty("current_session_context_class", "thread");
+            prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+
+            concreteSessionFactory = new org.hibernate.cfg.Configuration()
+                    .addProperties(prop)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory()
+            ;
+        } catch (Exception ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return concreteSessionFactory.openSession();
+    }
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -20,4 +56,5 @@ public class Util {
         }
         return connection;
     }
+
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc.dao;
 
+import jm.task.core.jdbc.bl.Util;
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,34 +16,102 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+
+            String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                    "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(100), " +
+                    "lastName VARCHAR(100), " +
+                    "age TINYINT" +
+                    ")";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
 
+            String sql = "DROP TABLE IF EXISTS users";
+
+            Query query = session.createSQLQuery(sql);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSession()) {
+            User user = new User(name, lastName, age);
+            session.beginTransaction();
 
+            session.save(user);
+
+            session.getTransaction().commit();
+            System.out.println("User с именем — " + name + " добавлен в базу данных");
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
 
+            session.delete((User)session.load(User.class,id));
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> data;
+        try (Session session = Util.getSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteria = builder.createQuery(User.class);
+            criteria.from(User.class);
+            data = session.createQuery(criteria).getResultList();
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+        return data;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+
+            String sql = "TRUNCATE TABLE users";
+
+            Query query = session.createSQLQuery(sql);
+            query.executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
